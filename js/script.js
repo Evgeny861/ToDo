7e7148bdeaa1166fe0a5c5db736c06138f56f38d
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-06-24 11:49:07 +0300

    Сделал удаление

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ const todoControl = document.querySelector('.todo-control'),
         headerInput = document.querySelector('.header-input'), 
         todoList = document.querySelector('.todo-list'),
         todoCompleted = document.querySelector('.todo-completed');
+        
 
             console.log(todoControl);
             console.log(headerInput);
             console.log(todoList);
             console.log(todoCompleted);
+            
             
             
         
@@ -35,11 +37,29 @@ const todoControl = document.querySelector('.todo-control'),
                 }
 
                 const btnTodoCompleted = li.querySelector('.todo-complete');
-
+                
                 btnTodoCompleted.addEventListener('click', function(){
                     item.completed = !item.completed;
                     render();
                 });
+                
+                console.log(item);
+                console.log(todoList);
+                console.log(li);
+                console.log(todoData.indexOf(''));
+                console.dir(todoData);
+                
+                
+                
+                
+                const btnTodoRemove = li.querySelector('.todo-remove');
+                btnTodoRemove.addEventListener('click', function(){
+                    
+                    todoData.splice(todoData.indexOf(item), 1);
+                    render();
+                });
+                
+                
             });
         };
 
@@ -51,16 +71,23 @@ const todoControl = document.querySelector('.todo-control'),
                 completed: false,
                 
             };
-            
+
             if(headerInput.value === ''){
                 alert('Введи название задачи');
             }else {
                 todoData.push(newTodo);
+                headerInput.value = '';
             }
-
+            
             
             render();
         });
 
 
         render();
+
+        // const btnStorage = document.querySelector('#add');
+        // btnStorage.addEventListener('click', function(){
+        //     localStorage.myText = headerInput.value;
+        // });
+        
